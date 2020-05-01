35392563bdc2a3993c8c79e2e86197270573da20
RamiTae <ramiTae.dev@gmail.com>
2020-05-01 23:38:07 +0900

    :recycle: Update code to use async await in category router

--- controller/category/index.js
+++ controller/category/index.js
@@ -5,12 +5,16 @@ module.exports = {
   list: require('./list'),
 
   //? 카테고리들의 목록을 받아옴 ( /category )
-  get: (req, res) => {
-    categories.findAll({ attributes: ['categoryName'] }).then(result => {
-      if (!result.length) {
+  get: async (req, res) => {
+    try {
+      const findCategories = await categories.findAll({ attributes: ['categoryName'] });
+
+      if (!findCategories.length) {
         return res.status(404).json('categories are empty');
       }
-      return res.status(200).json(result);
-    });
+      return res.status(200).json(findCategories);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
   },
 };
