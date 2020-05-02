9036c821ae302f48048d91e18cef9f99974a015a
RamiTae <ramiTae.dev@gmail.com>
2020-05-02 17:27:18 +0900

    :recycle: Update code to use async await in 'user' router

--- controller/user/logout.js
+++ controller/user/logout.js
@@ -1,16 +1,21 @@
 const { blacklist } = require('../../models');
 
 module.exports = {
-  post: (req, res) => {
+  post: async (req, res) => {
     const token = req.cookies.token;
     // console.log(token);
 
-    //? blacklist에 토큰 추가
-    blacklist
-      .create({ token })
-      .then(() => {
-        res.status(200).json('logout complete, add blacklist complete');
-      })
-      .catch(err => res.status(400).send(err));
+    try {
+      //? blacklist에 토큰 추가
+      const [, created] = await blacklist.create({ token });
+
+      if (!created) {
+        return res.status(422).json('already logout');
+      }
+
+      return res.status(200).json('logout complete, add blacklist complete');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
   },
 };
